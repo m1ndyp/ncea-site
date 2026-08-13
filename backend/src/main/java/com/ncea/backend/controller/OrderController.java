1d315a7e4b1e71c6b3ac5cdf3e6da7f9f4dcc473
m1ndyp <egorpirozhok10@gmail.com>
2026-08-13 17:46:22 +0200

    Сайт

--- backend/src/main/java/com/ncea/backend/controller/OrderController.java
+++ backend/src/main/java/com/ncea/backend/controller/OrderController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/orders")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class OrderController {
     @Autowired
     private OrderService orderService;
